82a504867e448e6d557d348484e122a271d1d973
Junior <161373637+ailtonjuniors2@users.noreply.github.com>
2025-06-29 13:24:33 -0300

    ajuste texto dos preços

--- utils.py
+++ utils.py
@@ -28,8 +28,9 @@ async def atualizar_dropdowns_estoque(bot: discord.ext.commands.Bot, guild: disc
 
             # Envia mensagem atualizada
             embed = discord.Embed(
-                title=f"⸝⸝ <:emoji_56:1388625033701625999> ┆Itens Disponíveis - {categoria}",
-                description="<:03_topico:1387904528929521737> Selecione um item abaixo para visualizar o preço e a quantidade atual em estoque. 𔓘",
+                title=f"                  ︶ ⏝ ︶ <:03_laco_amarelo:1387876036494233841> ︶ ⏝ ︶ \n"
+                      f"⊱ <:emoji_59:1388880400558063636> ⸝⸝ Itens Disponíveis - {categoria}",
+                description="<:03_topico:1387904528929521737> Selecione um item abaixo para visualizar o preço e a quantidade atual em estoque .ᐟ",
                 color=discord.Color.blue()
             )
 
